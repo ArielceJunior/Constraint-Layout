0b595225461c25ab0e5e04e2966bf6a9c80eb162
Arielce Júnior <arielce.junior@ifsuldeminas.edu.br>
2025-06-08 21:45:30 -0300

    First commit

---
+++ app/src/main/java/br/edu/ifsuldeminas/mch/constraintlayouts/BlogPostActivity.java
@@ -0,0 +1,21 @@
+package br.edu.ifsuldeminas.mch.constraintlayouts;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.constrictlayout.R;
+
+public class BlogPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_post);
+
+    }
+}
